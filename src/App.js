9cba6192509b2a78363029047756cd354b2679c2
Joonhart <64246924+Joonhart@users.noreply.github.com>
2023-05-07 16:48:42 +0900

    main part props render

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Outlet } from "react-router-dom";
 import LeftMenu from "./components/LeftMenu";
 import Header from "./components/Header";
 import { DarkModeProvider } from "./context/DarkModeContext";
+import { useState } from "react";
 
 function App() {
+  const [curMenu, setCurMenu] = useState("DASHBOARD");
   return (
     <DarkModeProvider>
     <div className="flex bg-white dark:bg-slate-800 dark:text-slate-300">
-      <LeftMenu />
+      <LeftMenu setCurMenu={setCurMenu}/>
       <main>
-      <Header />
+      <Header curMenu={curMenu}/>
       <Outlet />
       </main>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { useDarkMode } from '../context/DarkModeContext'
 import {HiMoon, HiSun} from 'react-icons/hi'
 
-export default function Header() {
+export default function Header({curMenu}) {
     const {darkMode, toggleDarkMode} = useDarkMode();
   return (
-    <header className='h-12 sticky top-0 flex justify-center'>
+    <header className='h-12 top-0 flex items-center text-xl'>
+        <div>
+          리즈델리 의 {curMenu}
+        </div>
+        <div className='text-2xl'>
         <button onClick={toggleDarkMode}>
             {!darkMode && <HiMoon />}
             {darkMode && <HiSun />}
         </button>
+        </div>
     </header>
   )
 }
