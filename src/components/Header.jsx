2785e0b7d2300b60a86bd7f4ae1d2a8c4862b77e
joonhart <joonhart@gmail.com>
2023-05-21 16:13:04 +0900

    add login alert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ import { HiMoon, HiSun } from "react-icons/hi";
 
 export default function Header({ curMenu }) {
   const { darkMode, toggleDarkMode } = useDarkMode();
+  const loginHandler = () => {
+    alert('서비스 준비 중 입니다.');
+  }
   return (
     <header className="h-12 p-4 py-10 w-full top-0 flex justify-between items-center text-lg lg:text-xl">
       <div className="text-2xl lg:text-4xl font-bold">리즈델리의 {curMenu}</div>
@@ -12,7 +15,7 @@ export default function Header({ curMenu }) {
           {!darkMode && <HiMoon />}
           {darkMode && <HiSun />}
         </button>
-        <button>
+        <button onClick={loginHandler}>
             LOGIN
         </button>
       </div>
